24bc2366f978297d6001f20e4617cbe69bd92cef
joel gs <joelpaginasweb@gmail.com>
2024-06-17 17:18:40 -0500

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $tasks= Task::latest()->paginate(3);
+        return view('index', ['tasks'=> $tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('create');
+    }
+ 
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //dd($request->all());
+
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'due_date' => 'required',
+            'status' => 'required'
+        ]);        
+
+        Task::create($request->all());
+
+        return redirect()->route('tasks.index')->with('success', 'Tarea creada');        
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit( $id): View
+    {
+        //dd($task);
+        $task = Task::find($id);
+        return view('edit', ['task' => $task]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task): RedirectResponse
+    {
+        //dd($request->all());
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required' //,
+           // 'due_date' => 'required',
+           // 'status' => 'required'
+        ]);
+
+        $task->update($request->all());
+        return redirect()->route('tasks.index')->with('success', 'Tarea actualizada');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task): RedirectResponse
+    {
+        //dd($task);
+        $task->delete();
+        return redirect()->route('tasks.index')->with('success', 'Tarea eliminada');
+    }
+}
